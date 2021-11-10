464201a71f83b62b70ee601ad3ccf2ad9b7f3cac
Rohit <rohitkumarankam@gmail.com>
2021-11-10 18:08:09 +0530

    added xterm.js

--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
 import './style.css'
 import Experience from './Experience/Experience.js'
+import '../node_modules/xterm/css/xterm.css'
+import './terminal.js'
 
 const experience = new Experience({
     targetElement: document.querySelector('.experience')
