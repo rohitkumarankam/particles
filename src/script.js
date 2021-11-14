97e3d41c6a169758c6531479873b112b39cb8304
Rohit <rohitkumarankam@gmail.com>
2021-11-14 14:50:03 +0530

    added gui and terminal modes

--- src/script.js
+++ src/script.js
@@ -1,8 +1,37 @@
 import './style.css'
 import Experience from './Experience/Experience.js'
 import '../node_modules/xterm/css/xterm.css'
-import './terminal.js'
+import terminalrun from './terminal.js'
 
-const experience = new Experience({
-    targetElement: document.querySelector('.experience')
-})
+// const experience = new Experience({
+//     targetElement: document.querySelector('.experience')
+// })
+var body = document.querySelector('body')
+document.querySelector('.gui').addEventListener('click', ()=>{console.log('gui'); removechield(body); addgui(body)})
+document.querySelector('.terminal').addEventListener('click', ()=>{console.log('terminal'); removechield(body); addterminal(body)})
+
+function removechield(element){
+    while(element.firstChild){
+        element.removeChild(element.firstChild)
+    }
+}
+function addgui(element){
+    var gui = document.createElement('h1')
+    gui.classList.add('gui')
+    gui.innerText = 'gui'
+    gui.style.color = '#fff'
+    var exp = document.createElement('div')
+    exp.classList.add('experience')
+    element.appendChild(exp)
+    element.appendChild(gui)
+    // import Experience from './Experience/Experience.js'
+    const experience = new Experience({
+        targetElement: document.querySelector('.experience')
+    })
+}
+function addterminal(element){
+    var Terminal = document.createElement('div')
+    Terminal.id = 'terminal'
+    element.appendChild(Terminal)
+    terminalrun()
+}
